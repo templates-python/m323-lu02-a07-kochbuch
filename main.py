a0af6fafc3f7bf5049a620b2e2fb1155d3f565a9
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-30 16:04:17 +0200

    done

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 # Dein Code kommt hier hin
+import json
+
+def adjust_recipe(original_recipe, num_people):
+    """
+    Adjusts the recipe based on the number of people.
+
+    Parameters:
+        original_recipe (dict): The original recipe.
+        num_people (int): The number of people to adjust the recipe for.
+
+    Returns:
+        dict: The adjusted recipe.
+    """
+    adjusted_ingredients = {}
+    original_servings = original_recipe['servings']
+    factor = num_people / original_servings
+
+    for ingredient, amount in original_recipe['ingredients'].items():
+        adjusted_ingredients[ingredient] = amount * factor
+
+    adjusted_recipe = {
+        'title': original_recipe['title'],
+        'ingredients': adjusted_ingredients,
+        'servings': num_people
+    }
+    return adjusted_recipe
+
+def load_recipe(json_string):
+    """
+    Loads a recipe from a JSON string.
+
+    Parameters:
+        json_string (str): The JSON string containing the recipe.
+
+    Returns:
+        dict: The recipe as a Python dictionary.
+    """
+    return json.loads(json_string)
 
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
+    # Example JSON string for a recipe
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+    # Convert JSON string to Python dictionary
+    original_recipe = load_recipe(recipe_json)
+    # Adjust the recipe for 2 people
+    adjusted_recipe = adjust_recipe(original_recipe, 8)
+    print(f"Original Recipe: {original_recipe}")
+    print(f"Adjusted Recipe: {adjusted_recipe}")
