e43d30a7ada232a64f88db138ff35970d249a9f5
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:03:35 +0200

    Ran pyink on branch main

--- main_test.py
+++ main_test.py
@@ -1,12 +1,26 @@
 import main
 
+
 def test_adjust_recipe():
-    original_recipe = {"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}
+    original_recipe = {
+        "title": "Spaghetti Bolognese",
+        "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500},
+        "servings": 4,
+    }
     num_people = 2
     adjusted_recipe = main.adjust_recipe(original_recipe, num_people)
-    assert adjusted_recipe == {"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 200, "Tomato Sauce": 150, "Minced Meat": 250}, "servings": 2}
+    assert adjusted_recipe == {
+        "title": "Spaghetti Bolognese",
+        "ingredients": {"Spaghetti": 200, "Tomato Sauce": 150, "Minced Meat": 250},
+        "servings": 2,
+    }
+
 
 def test_load_recipe():
     json_string = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
     loaded_recipe = main.load_recipe(json_string)
-    assert loaded_recipe == {"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}
+    assert loaded_recipe == {
+        "title": "Spaghetti Bolognese",
+        "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500},
+        "servings": 4,
+    }
